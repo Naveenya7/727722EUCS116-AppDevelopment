523090f913f6258d29f0e6eb3f0c2fbda467bd3f
Naveenya <115526605+Naveenya7@users.noreply.github.com>
2024-07-30 09:29:01 +0530

    Add files via upload

---
+++ Day2/src/components/loginandsignup/RegisterType.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './RegisterType.css';
+
+const RegisterType = () => {
+    return (
+        <div className="register-type-container">
+            <h2>Register As</h2>
+            <div className="register-options">
+                <Link to="/register/client" className="register-option client-option">
+                    <div className="option-icon">
+                        <i className="fas fa-user"></i>
+                    </div>
+                    <div className="option-text">Client</div>
+                </Link>
+                <Link to="/register/provider" className="register-option provider-option">
+                    <div className="option-icon">
+                        <i className="fas fa-truck"></i>
+                    </div>
+                    <div className="option-text">Transportation Provider</div>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterType;
