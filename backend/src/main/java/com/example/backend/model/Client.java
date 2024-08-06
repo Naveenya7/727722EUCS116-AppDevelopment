e042c3cbc1bcae2995cfe33020cb663d94d45ce7
Naveenya <115526605+Naveenya7@users.noreply.github.com>
2024-08-06 14:04:38 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/model/Client.java
@@ -0,0 +1,32 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String companyName;
+}
