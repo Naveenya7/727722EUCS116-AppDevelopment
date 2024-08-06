c462ce275edb8c545de9cbbb88928331c6064b40
Naveenya <115526605+Naveenya7@users.noreply.github.com>
2024-08-06 15:23:30 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/CityVehicleDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.backend.model.CityVehicleDetail;
-
-@Repository
-public interface CityVehicleDetailRepository extends JpaRepository<CityVehicleDetail, Long> {
-}
