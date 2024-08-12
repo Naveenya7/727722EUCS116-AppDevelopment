8997ebf52ec5c75644a8614f80e29182409675ef
Naveenya <115526605+Naveenya7@users.noreply.github.com>
2024-08-12 20:37:28 +0530

    Add files via upload

---
+++ Day-5/src/components/landingpage/LandingPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import HeroSection from "./Hero";
+import FeaturesSection from "./Features";
+import Footer from "./Footer";
+import "./LandingPage.css";
+import TransportationPartners from "./Transportationpartner";
+import Navbar from "./Navbar";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <Navbar />
+
+      <HeroSection />
+      <FeaturesSection />
+      <TransportationPartners />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
